dbe6127b2b5c3066196d405187917efb31a7079e
Cat Rivers <cat.rivers.blog@gmail.com>
2023-05-03 15:09:40 +0300

    Group sizez basic exercise

---
+++ 1_lecture/1.12-assignment.js
@@ -0,0 +1,40 @@
+
+
+const numPeople = process.argv[2]
+const sizeOfGroup = process.argv[3]
+
+//basic solution
+const groupSizeOne = (people, size) => {
+  const result = Math.ceil(people / size);
+  console.log(`number of groups: ${result}`);
+};
+
+//Basic solution without Math library
+
+const groupSizeTwo = (people, size) => {
+  const groups = people / size;
+  let result;
+  const leftover = groups % 1;
+   leftover ? (result = groups - leftover + 1) : (result = groups);
+
+  console.log(`number of groups: ${result}`);
+};
+
+
+
+
+//Extra...not completed
+const groupSizeThree = (people, size) => {
+  const div = people/size
+  const mod = div % size
+  const  round = ((1 - (div % 1)) + mod)//logic flawed, review algorithm
+  const result = (div - mod) + round
+
+  console.log(`Number of groups: ${result}`)
+}
+
+
+groupSizeOne(numPeople, sizeOfGroup)
+groupSizeTwo(numPeople, sizeOfGroup)
+groupSizeThree(numPeople, sizeOfGroup)
+

0034f921adcfe55378d17e09bdc05db11c993dad
Cat Rivers <cat.rivers.blog@gmail.com>
2023-05-10 07:23:28 +0300

    atm conditional

---
+++ 3_lecture/3.14-assignment.ts
@@ -0,0 +1,27 @@
+// ATM Machine
+
+const balance = 0
+const isActive = false;
+const checkBalance = false;
+
+
+if (checkBalance) {
+    if (isActive) {
+        if (balance > 0) {
+            console.log("balance: " + balance + " €");
+        } 
+        else if (balance < 0) {
+            console.log("your account is negative");
+            
+        } 
+        else {
+            console.log("Your account is empty");
+        }
+    } 
+    else {
+        console.log("your account is not active");
+    }
+} 
+else {
+    console.log("Have a nice day!");
+}

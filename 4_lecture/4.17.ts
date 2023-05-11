2118f6ff8a96ae1b8af7159a394bad2a8fd04c40
Cat Rivers <cat.rivers.blog@gmail.com>
2023-05-11 11:53:37 +0300

    add assignment 4.17

---
+++ 4_lecture/4.17.ts
@@ -0,0 +1,14 @@
+// Reverse words
+//! run command line --> node dist/4.17.js <"arg1"> 
+
+
+const phrase = process.argv[2]
+
+
+const reversedPhrase = phrase
+  .split(" ")
+  .map(el => el.split("").reverse().join(""))
+  .join(" ");
+
+
+console.log(reversedPhrase)

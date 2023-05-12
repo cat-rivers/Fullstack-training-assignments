e0157adc1e8f24b925ea1ffec7f364c8ce1e8e6d
Cat Rivers <cat.rivers.blog@gmail.com>
2023-05-12 08:41:47 +0300

    prime numbers 4.21

--- 4_lecture/4.21.ts
+++ 4_lecture/4.21.ts
@@ -1,2 +1,28 @@
 //Prime numbers
 
+function isPrime(num: number){
+  let divBy: number[] = []
+  for(let i = 1; i <= num ; i++ ){
+  if(num % i === 0){
+    divBy.push(i)
+  }
+}
+if(divBy.length === 2){
+  console.log(`${num} is a prime number`)
+}else{
+  console.log(`${num} is not a prime number`)
+
+}}
+
+//primes
+isPrime(83)
+isPrime(89)
+isPrime(97)
+
+//not primes
+isPrime(4)
+isPrime(6)
+isPrime(8)
+isPrime(9)
+isPrime(12)
+isPrime(10)

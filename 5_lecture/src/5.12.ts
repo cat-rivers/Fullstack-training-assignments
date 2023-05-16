cd17228ddd0105c8e0ce9911072ab06bcbe0e199
Cat Rivers <cat.rivers.blog@gmail.com>
2023-05-16 20:04:14 +0300

    assignment 5.12

---
+++ 5_lecture/src/5.12.ts
@@ -0,0 +1,21 @@
+//Count vowels
+
+function getVowelCount(string: string) {
+    let count = 0;
+    const vowels = "aeiouy";
+
+    for (const char of string) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count === 0 ? "NO VOWELS FOUND" : count
+}
+    
+
+
+console.log(getVowelCount("aaaabbbbccceee"))
+console.log(getVowelCount("bbb"))
+console.log(getVowelCount("aeiouy"))
+console.log(getVowelCount("dddddi"))
+console.log(getVowelCount("brekfesty"))

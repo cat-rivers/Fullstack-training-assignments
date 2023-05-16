f6649f1e8546e01a6c4478f3002a2028717a931d
Cat Rivers <cat.rivers.blog@gmail.com>
2023-05-16 19:43:37 +0300

    5.10 version with string literals

--- 5_lecture/src/5.10.ts
+++ 5_lecture/src/5.10.ts
@@ -2,17 +2,16 @@
 
 function likes(names: string[]) {
     const numNames = names.length;
-  
     if (numNames === 0) {
         return "no one likes this";
     } else if (numNames === 1) {
-        return names[0] + " likes this";
+        return `${names[0]} likes this`;
     } else if (numNames === 2) {
-        return names[0] + " and " + names[1] + " like this";
+        return `${names[0]} and ${names[1]} like this`;
     } else if (numNames === 3) {
-        return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     } else {
-        return names[0] + ", " + names[1] + " and " + (numNames - 2) + " others like this";
+        return `${names[0]}, ${names[1]} and ${numNames - 2} others like this`;
     }
 }
 

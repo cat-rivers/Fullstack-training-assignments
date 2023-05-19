10204d43a04ea3165fce4359cda1e69dcc4edf50
Cat Rivers <cat.rivers.blog@gmail.com>
2023-05-19 14:29:05 +0300

    ci test 9

--- 5.1-assignment/src/index.ts
+++ 5.1-assignment/src/index.ts
@@ -1,5 +1,8 @@
 //TODO create the pipeline again with this folder
-
+//Doesnt pass the pipeline ci stage : lint.
+//! error message: 
+//! ESLint couldn't determine the plugin "@typescript-eslint" uniquely.
+// deleting script for now
 
 console.log("Success")
 let hello = "hello;"

3e8771769b102a333f74ec90f32f289850c26572
Catalina Riveros <cat.rivers.blog@gmail.com>
2023-05-23 09:43:17 +0300

    bleh2

---
+++ 6_lecture/src/6.9.ts
@@ -0,0 +1,11 @@
+//Race cars
+
+//function : raceLap() --> returns promise(rej or res)
+//1. does it crash? yes no
+//2. time between 20-25
+//rejected / res = lap time
+
+//function race(list of drivers, number of laps) it runs the racelap()for each racer on each lap
+//race keeps track of each drivers time for each lap and adds them
+//returns winner name and stats
+

5040018a1d7cc6322e4a981142d24ab121d221c7
Cat Rivers <cat.rivers.blog@gmail.com>
2023-05-30 16:58:08 +0300

    .

---
+++ 9_lecture/9.1-9.5/9.1/middleware.ts
@@ -0,0 +1,46 @@
+
+import {Request, Response, NextFunction} from 'express'
+
+export const logger = (req: Request, res: Response, next: NextFunction) => {
+  const reqTime = new Date();
+  const method = req.method;
+  const url = req.url;
+  console.log({ requestTime: reqTime, method: method, requestUrl: url }, req.body);
+  next();
+};
+
+
+export const endPointNotFound = (req: Request, res: Response) => {
+  res.status(404).send({ error: "oops...wrong place maybe?" });
+
+};
+
+
+export const checkParams = (req: Request, res: Response, next: NextFunction) => {
+  const { name, id, email } = req.body
+    if (!id || !name || !email)  {
+        return res.status(400).send('Error: Missing or invalid parameters')
+    }
+    next()
+}
+
+export const checkPartialParams = (req: Request, res: Response, next: NextFunction) => {
+  const { name, id, email } = req.body
+    if ( !name && !email )  {
+        return res.status(400).send('Error: Missing Email and Name')
+    }
+    if ( id )  {
+      return res.status(400).send('Error: Id cannot be updated')
+  }
+    next()
+}
+
+export const checkId = (req: Request, res: Response, next: NextFunction) => {
+  const { id} = req.params
+ 
+  if(!id || Number.isNaN(parseInt(id))){
+    return res.status(400).send("gimme a number u fish")
+
+  }
+  next()
+}

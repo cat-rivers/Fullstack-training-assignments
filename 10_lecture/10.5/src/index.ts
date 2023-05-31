4cfd2de9ba89239051ca73b0299322880d3b4f9c
Catalina Riveros <cat.rivers.blog@gmail.com>
2023-05-31 15:05:32 +0300

    lecture 10 class assignments

---
+++ 10_lecture/10.5/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+
+const app =  express(); 
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Docker')
+})
+
+
+
+app.listen(3000 , ()=> console.log("Listening"))
